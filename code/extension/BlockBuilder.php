6b565c49c1225091da575989cb3a796f8cfc2f93
timairey <timairey87@googlemail.com>
2015-07-10 12:17:00 +0100

    Added config API hook
    Now looks for the variable UseBlockBuilder in the config API for the
    calling class (page class).
    
    Want to update the listing feature to use the config API to allow
    control over which blocks are available.

--- code/extension/BlockBuilder.php
+++ code/extension/BlockBuilder.php
@@ -9,23 +9,21 @@ class BlockBuilder extends DataExtension {
 
     private static $has_many = array(
         "PageBlocks"    => 'PageBlock'
-        // "BlockRows"         => 'BlockRow'
     );
  
-    public function updateCMSFields(FieldList $fields) {
-        // $fields->addFieldToTab("Root.Social", new TextField("FaceBookLink"));
-        // $fields->addFieldToTab("Root.Social", new TextField("TwitterLink"));
-        $fields->removeByName("Content");
-        // echo '<pre>' . print_r($this->owner, true) . '</pre>';
-        // die();
-                     
-        $gridFieldConfig = GridFieldConfig_PageBlock::create();   
-        $gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
-        $blocks = DataObject::get("PageBlock")->filter(array("PageID" => $this->owner->ID))->sort("SortOrder");
-        $grid = new GridField('PageBlocks','PageBlocks', $blocks ,$gridFieldConfig); 
-        $grid->getConfig()->getComponentByType('GridFieldDetailForm')->setValidator(singleton('PageBlock')->getCMSValidator()); 
-        $fields->addFieldToTab('Root.Main', $grid, "Metadata");
-
+    public function updateCMSFields(FieldList $fields) {                 
+        // only show block bulder on selected pages
+        // if all pages want block builder, then set the var on SiteTree.
+        if ($this->owner->config()->UseBlockBuilder){
+            $fields->removeByName("Content");
+                        
+            $gridFieldConfig = GridFieldConfig_PageBlock::create();   
+            $gridFieldConfig->addComponent(new GridFieldSortableRows('SortOrder'));
+            $blocks = DataObject::get("PageBlock")->filter(array("PageID" => $this->owner->ID))->sort("SortOrder");
+            $grid = new GridField('PageBlocks','PageBlocks', $blocks ,$gridFieldConfig); 
+            $grid->getConfig()->getComponentByType('GridFieldDetailForm')->setValidator(singleton('PageBlock')->getCMSValidator()); 
+            $fields->addFieldToTab('Root.Main', $grid, "Metadata");
+        }
     }
 
 }
